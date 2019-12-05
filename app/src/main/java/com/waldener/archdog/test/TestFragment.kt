c625f2dbf92f6b29d8db97a0570d1871945ca438
WaldenerLee <waldenerlee@gmail.com>
2019-12-05 14:16:54 +0800

    Update TestFragment.kt

--- app/src/main/java/com/waldener/archdog/test/TestFragment.kt
+++ app/src/main/java/com/waldener/archdog/test/TestFragment.kt
@@ -7,8 +7,9 @@ import android.view.ViewGroup
 
 import com.waldener.archdog.R
 import com.waldener.arch.ArchFragment
+import com.waldener.arch.ArchViewModel
 
-class TestFragment : ArchFragment<TestVM>() {
+class TestFragment : ArchFragment<ArchViewModel>() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_test, container, false)
